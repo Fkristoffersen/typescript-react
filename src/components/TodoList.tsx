e09f8491fd716d0b5dae651e4361e9d220dd49e8
Frederik Kristoffersen <Frederikkristoffersen95@gmail.com>
2022-09-06 08:33:49 +0200

    TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css";
+
+interface Props{
+    todos:Todo[];
+    setTodos:
+}
+
+const TodoList: React.FC = (todos,setTodos) => {
+    return <div className='todos'></div>
+};
+
+export default TodoList;
